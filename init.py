82050eeb8ac3525eedbcdb07797e84cb23a8ea48
Matheus Zauza Maschietto <matheus_maschietto@yahoo.com.br>
2025-10-25 10:56:57 -0300

    refactoring prediction_service

--- init.py
+++ init.py
@@ -49,52 +49,21 @@ for i in range(len(testing_metrics)):
     vector = NumpyService.to_float64_list(testing_metrics.iloc[i])
     testing_vectors.append(vector)
 
-correct_predictions = 0
-total_predictions = len(testing_vectors)
-human1_correct = 0
-human1_incorrect = 0
-human2_correct = 0
-human2_incorrect = 0
-
-for i, test_vector in enumerate(testing_vectors):
-    predicted_author = prediction_service.predict_author(test_vector)
-
-    if predicted_author is None:
-        continue
-
-    actual_author = testing_messages[i]['nomePessoa']
-    
-    if predicted_author == actual_author:
-        correct_predictions += 1
-        if actual_author == 'Human 1':
-            human1_correct += 1
-        elif actual_author == 'Human 2':
-            human2_correct += 1
-    else:
-        if actual_author == 'Human 1':
-            human1_incorrect += 1
-        elif actual_author == 'Human 2':
-            human2_incorrect += 1
-    
-accuracy = (correct_predictions / total_predictions) * 100
-incorrect_predictions = total_predictions - correct_predictions
-
-human1_actual = sum(1 for msg in testing_messages if msg['nomePessoa'] == 'Human 1')
-human2_actual = sum(1 for msg in testing_messages if msg['nomePessoa'] == 'Human 2')
+results = prediction_service.evaluate_predictions(testing_vectors, testing_messages)
 
 print(f"\nGerando gráficos de visualização...")
 VisualizationService.create_accuracy_bar_chart(
-    correct_predictions=correct_predictions,
-    incorrect_predictions=incorrect_predictions,
+    correct_predictions=results['correct_predictions'],
+    incorrect_predictions=results['incorrect_predictions'],
     output_path='./accuracy_chart.png'
 )
 
 VisualizationService.create_detailed_bar_chart(
-    correct_predictions=correct_predictions,
-    incorrect_predictions=incorrect_predictions,
-    human1_correct=human1_correct,
-    human1_incorrect=human1_incorrect,
-    human2_correct=human2_correct,
-    human2_incorrect=human2_incorrect,
+    correct_predictions=results['correct_predictions'],
+    incorrect_predictions=results['incorrect_predictions'],
+    human1_correct=results['human1_correct'],
+    human1_incorrect=results['human1_incorrect'],
+    human2_correct=results['human2_correct'],
+    human2_incorrect=results['human2_incorrect'],
     output_path='./detailed_accuracy_chart.png'
 )
--- prediction_service.py
+++ prediction_service.py
@@ -12,3 +12,50 @@ class PredictionService:
     
     def is_correct_prediction(self, predicted_author: str, actual_author: str) -> bool:
         return predicted_author == actual_author
+    
+    def evaluate_predictions(self, testing_vectors: list, testing_messages: list) -> dict:
+        correct_predictions = 0
+        total_predictions = len(testing_vectors)
+        human1_correct = 0
+        human1_incorrect = 0
+        human2_correct = 0
+        human2_incorrect = 0
+
+        for i, test_vector in enumerate(testing_vectors):
+            predicted_author = self.predict_author(test_vector)
+
+            if predicted_author is None:
+                continue
+
+            actual_author = testing_messages[i]['nomePessoa']
+            
+            if predicted_author == actual_author:
+                correct_predictions += 1
+                if actual_author == 'Human 1':
+                    human1_correct += 1
+                elif actual_author == 'Human 2':
+                    human2_correct += 1
+            else:
+                if actual_author == 'Human 1':
+                    human1_incorrect += 1
+                elif actual_author == 'Human 2':
+                    human2_incorrect += 1
+        
+        accuracy = (correct_predictions / total_predictions) * 100
+        incorrect_predictions = total_predictions - correct_predictions
+
+        human1_actual = sum(1 for msg in testing_messages if msg['nomePessoa'] == 'Human 1')
+        human2_actual = sum(1 for msg in testing_messages if msg['nomePessoa'] == 'Human 2')
+        
+        return {
+            'correct_predictions': correct_predictions,
+            'total_predictions': total_predictions,
+            'incorrect_predictions': incorrect_predictions,
+            'accuracy': accuracy,
+            'human1_correct': human1_correct,
+            'human1_incorrect': human1_incorrect,
+            'human2_correct': human2_correct,
+            'human2_incorrect': human2_incorrect,
+            'human1_actual': human1_actual,
+            'human2_actual': human2_actual
+        }
